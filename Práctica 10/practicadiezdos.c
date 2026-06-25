751e6a9d54bbd7b66b4cd2ef9feca425a12aac9e
Ise515 <mariai.varela@udem.edu>
2026-06-25 11:56:52 -0600

    Add files via upload

---
+++ Práctica 10/practicadiezdos.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>         // Biblioteca principal del compilador XC8
+#include <stdbool.h>
+#include "lcd.h"
+//=============================================================================
+// CONFIGURACION DE BITS DE CONFIGURACION (FUSES)
+//=============================================================================
+
+// Seleccion de oscilador (usar XT si estas usando un cristal de 8 MHz)
+#pragma config FOSC = HS        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (enabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (disabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection (disabled)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable (disabled)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (disabled)
+
+//=============================================================================
+// DEFINICIONES
+//=============================================================================
+
+#define _XTAL_FREQ 8000000      // Frecuencia del oscilador (para __delay_ms y __delay_us)
+
+volatile unsigned int tiempo = 0;
+volatile unsigned char contador = 0;
+
+char txtVolt[10];
+char txtTiempo[6];
+
+void ADC_Init(void){
+    ADCON0 = 0x41; 
+    ADCON1 = 0x80;   
+}
+
+unsigned int ADC_Read(void){
+    GO_nDONE = 1;
+    while(GO_nDONE);
+
+    return ((ADRESH << 8) + ADRESL);
+}
+
+void Timer1_Init(void){
+    T1CON = 0b00110001;
+
+    TMR1H = 0xCF;
+    TMR1L = 0x2C;
+
+    TMR1IF = 0;
+    TMR1IE = 1;
+
+    PEIE = 1;
+    GIE = 1;
+}
+
+void __interrupt() ISR(void){
+    if(TMR1IF){
+        contador++;
+
+        if(contador >= 20){
+            tiempo++;
+            contador = 0;
+        }
+
+        TMR1H = 0xCF;
+        TMR1L = 0x2C;
+
+        TMR1IF = 0;
+    }
+}
+
+void main(void){
+    float voltaje;
+    unsigned int adc;
+
+    TRISA0 = 1;
+
+    ADC_Init();
+    Timer1_Init();
+
+    LCD lcd = {&PORTC,2,3,4,5,6,7};
+    LCD_Init(lcd);
+    LCD_Clear();
+
+    while(1){
+        adc = ADC_Read();
+        voltaje = adc * 5.0 / 1023.0;
+
+        // Voltaje (izquierda))
+        LCD_Set_Cursor(0,0);
+        sprintf(txtVolt,"V:%1.2f",voltaje);
+        LCD_putrs(txtVolt);
+
+        // Tiempo (derecha))
+        LCD_Set_Cursor(1,11);
+        sprintf(txtTiempo,"%02u:%02u",
+                tiempo/60,
+                tiempo%60);
+        LCD_putrs(txtTiempo);
+
+        __delay_ms(100);
+    }
+}
